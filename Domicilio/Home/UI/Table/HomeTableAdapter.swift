1a0c5a8c83d151c788f712a2b9bffead2fe41ee4
Christian Miranti <christian.miranti@gmail.com>
2020-04-05 12:25:26 +0200

    Add header in sections

--- Domicilio/Home/UI/Table/HomeTableAdapter.swift
+++ Domicilio/Home/UI/Table/HomeTableAdapter.swift
@@ -21,10 +21,17 @@ extension HomeTableAdapter {
 }
 
 extension HomeTableAdapter: UITableViewDataSource {
+    // Headers and sections
     func numberOfSections(in tableView: UITableView) -> Int {
         viewState?.count ?? 0
     }
     
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        guard let group = viewState?[section] else { fatalError("Can't have an out of index here") }
+        return group.icon + " " + group.name
+    }
+    
+    // Cells and rows
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         viewState?[section].activities.count ?? 0
     }
